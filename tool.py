0729d61d8478ffb8db148aac149e8c50b88f0f06
GDNDZZK <10382956+gdnd-zzk@user.noreply.gitee.com>
2024-03-17 13:01:29 +0800

    tool工具优化,适应不同终端宽度

--- tool.py
+++ tool.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 import sys
-
+import os
 from util.keyboardListener import KeyboardListener
 
 
 def printByLine(key_code_num):
+    # 获取终端宽度
+    size = os.get_terminal_size().columns
     # 通过打印一个换行符回到终端的开始
     sys.stdout.write('\r')
     # 清除到行尾的内容
-    sys.stdout.write(' ' * 80)
+    sys.stdout.write(' ' * size)
     # 再次回到行首
     sys.stdout.write('\r')
     sys.stdout.write('+'.join([str(t) for t in key_code_num]))
